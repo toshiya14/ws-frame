090842fe569e4a1a77c30ca573b4a04ea02e0966
Akaishi Toshiya <Toshiya14@live.jp>
2021-11-04 15:49:45 +0800

    Add test server project.

--- ws-frame.js
+++ ws-frame.js
@@ -1,4 +1,4 @@
-import { EventHandler } from "./events";
+import EventHandler from "./events";
 
 const EVT_LOST = "lost";
 const EVT_INITED = "inited";
@@ -6,6 +6,7 @@ const EVT_CLOSED = "closed";
 const EVT_TIMEOUT = "timeout";
 const EVT_RECEIVED = "received";
 const EVT_CONNECTED = "connected";
+const EVT_RECONNECTED = "reconnected";
 const EVT_TRYTORECONNECT = "try-to-reconnect";
 const CON_STATE_LOST = "lost";
 const CON_STATE_CLOSED = "closed";
@@ -18,11 +19,14 @@ const LNE_STATE_ONLINE = "online";
 const LNE_STATE_OFFLINE = "offline";
 const TYPE_FUNCTION = "function";
 
+let ton_SendPingTask, ton_ConnectionTimeout, ton_Reconnect;
+let manuallyClose = false;
+
 export default function (options) {
   let _opt = Object.assign(
     {
       debugLog: false,
-      connectTimeout: 10000,
+      connectTimeout: 5000,
       reconnect: true,
       reconnectDelay: 5000,
       sendPing: true,
@@ -43,7 +47,6 @@ export default function (options) {
     inited: false
   };
   this.$evt = new EventHandler();
-  this._timeoutForConnect = null;
 
   let debugStageLog = (stage, data) => {
     if (this.$options.debugLog) {
@@ -58,22 +61,41 @@ export default function (options) {
   };
 
   let sendPingTask = () => {
-    if (this.$wsc && this.$wsc.readyState === 1 && this.onlineState === LNE_STATE_ONLINE) {
-      if (this.$options && typeof this.$options.generatePingPack === TYPE_FUNCTION) {
+    // If there is a planned task, clear it.
+    if (ton_SendPingTask) {
+      stopSendPing();
+    }
+
+    debugStageLog("prepare send ping", [`readyState:${this.$wsc.readyState}`, `onlineState:${this.$props.onlineState}`, `sendPing:${this.$options.sendPing}`]);
+    if (this.$wsc && this.$wsc.readyState === 1 && this.$props.onlineState === LNE_STATE_ONLINE) {
+      if (this.$options.sendPing && typeof this.$options.generatePingPack === TYPE_FUNCTION) {
         let pingPack = this.$options.generatePingPack();
         this.$wsc.send(pingPack);
         this.$props.lastPingTime = new Date();
         debugStageLog("sendPing", [`lastPingTime:${this.$props.lastPingTime.toLocaleString()}`]);
       }
     }
-    setTimeout(sendPingTask, sendPingPeriod);
+    ton_SendPingTask = setTimeout(sendPingTask, this.$options.sendPingPeriod);
+  };
+
+  let stopSendPing = () => {
+    clearTimeout(ton_SendPingTask);
   };
 
   let connectionCheckingTask = () => {
-    if (this.$wsc && this.$wsc.readyState === 1 && this.onlineState === LNE_STATE_ONLINE) {
+    if (!this.$options.sendPing) {
+      return; // if not send ping, do not check connection delay.
+    }
+
+    if (this.$wsc && this.$wsc.readyState === 1 && this.$props.onlineState === LNE_STATE_ONLINE) {
       let lastPingTime = this.$props.lastPingTime;
       let lastPongTime = this.$props.lastPongTime;
       let currentTime = new Date();
+
+      // not sent ping yet.
+      if (!lastPingTime) {
+        return; // not sent ping yet, do not check state.
+      }
 
       // stand-by phase.
       if (lastPingTime < lastPongTime) {
@@ -88,7 +110,6 @@ export default function (options) {
           this.$props.onlineState = LNE_STATE_OFFLINE;
           this.$wsc.close(); // Bad-communication.
           debugStageLog("badCommunicationDetected");
-
           // `lost` event would be triggered.
           this.$evt.fire(EVT_LOST, {
             wsc: this.$wsc
@@ -101,16 +122,44 @@ export default function (options) {
   };
 
   let onWebSocketReconnect = () => {
+    // if a reconnect task is scheduled, skip.
+    if (ton_Reconnect != null) {
+      return;
+    }
+
     // `try-to-reconnect` event would be triggered.
     this.$evt.fire(EVT_TRYTORECONNECT, {
-      wsc: this.$wsc
+      wsc: this.$wsc,
+      delay: this.$options.reconnectDelay
     });
     debugStageLog("tryToReconnect");
 
-    initWebSocket();
+    if (ton_ConnectionTimeout) {
+      clearTimeout(ton_ConnectionTimeout);
+      ton_ConnectionTimeout = null;
+    }
+
+    if (ton_Reconnect) {
+      clearTimeout(ton_Reconnect);
+    }
+    ton_Reconnect = setTimeout(initWebSocket, this.$options.reconnectDelay);
   };
 
   let initWebSocket = () => {
+    // clear timeout
+    if (ton_Reconnect) {
+      clearTimeout(ton_Reconnect);
+      ton_Reconnect = null;
+    }
+
+    if (ton_ConnectionTimeout) {
+      clearTimeout(ton_ConnectionTimeout);
+      ton_ConnectionTimeout = null;
+    }
+    // clear timer
+    this.$props.lastPingTime = null;
+    this.$props.lastPongTime = null;
+
     if ("WebSocket" in window) {
       if (this.$options.server) {
         try {
@@ -118,8 +167,33 @@ export default function (options) {
 
           this.$wsc = new WebSocket(this.$options.server);
 
+          // if the connection timeout.
+          ton_ConnectionTimeout = setTimeout(() => {
+            // if the WebSocket is connected or connecting,
+            // close it.
+            if (this.$wsc && (this.$wsc.readyState === 0 || this.$wsc.readyState === 1)) {
+              this.$wsc.close();
+            }
+            this.$wsc = null;
+            this.$props.connectState = CON_STATE_TIMEOUT;
+            this.$props.onlineState = LNE_STATE_OFFLINE;
+
+            // `timeout` event would be triggered.
+            // if the connection open failed.
+            this.$evt.fire(EVT_TIMEOUT, {
+              server: this.$options.server,
+              wsc: this.$wsc
+            });
+
+            // Performs a delayed reconnection.
+            if (this.$options.reconnect) {
+              onWebSocketReconnect();
+            }
+            debugStageLog("WebSocketClient timeout");
+          }, this.$options.connectTimeout);
+
           // Bind event to `open` for `WebSocketClient`.
-          this.$wsc.onopen = function () {
+          this.$wsc.onopen = () => {
             // readyState check.
             // the readyState currently could be only 1.
             if (this.$wsc.readyState !== 1) {
@@ -132,8 +206,9 @@ export default function (options) {
             }
 
             // clear the connection timeout counter.
-            clearTimeout(this._timeoutForConnect);
-            debugStageLog("WebSocketClient connected");
+            clearTimeout(ton_ConnectionTimeout);
+
+            console.log("opened and state", this.$props.connectState, this.$props.inited);
 
             // The first time to connect to the server.
             if (!this.$props.inited) {
@@ -148,10 +223,10 @@ export default function (options) {
             }
 
             // The connection once been lost.
-            else if (this.$props.connectState === CON_STATE_RECONNECTING) {
+            else {
               this.$props.connectState = CON_STATE_CONNECTED;
               this.$props.onlineState = LNE_STATE_ONLINE;
-              this.$evt.fire(CON_STATE_RECONNECTED, {
+              this.$evt.fire(EVT_RECONNECTED, {
                 server: this.$options.server,
                 wsc: this.$wsc
               });
@@ -164,51 +239,38 @@ export default function (options) {
               server: this.$options.server,
               wsc: this.$wsc
             });
+            debugStageLog("WebSocketClient connected");
 
-            this._timeoutForConnect = setTimeout(() => {
-              // if the WebSocket is connected or connecting,
-              // close it.
-              if (this.$wsc.readyState === 0 || this.$wsc.readyState === 1) {
-                this.$wsc.close();
-              }
-              this.$wsc = null;
-              this.$props.connectState = CON_STATE_TIMEOUT;
-              this.$props.onlineState = LNE_STATE_OFFLINE;
-
-              // `timeout` event would be triggered.
-              // if the connection open failed.
-              this.$evt.fire(EVT_TIMEOUT, {
-                server: this.$options.server,
-                wsc: this.$wsc
-              });
-
-              // Performs a delayed reconnection.
-              if (this.$options.reconnect) {
-                setTimeout(onWebSocketReconnect, this.reconnectDelay);
-              }
-            }, this.$options.connectTimeout);
-
-            debugStageLog("WebSocketClient timeout");
+            // start to send ping.
+            if (this.$options.sendPing) {
+              sendPingTask();
+            }
           };
 
           // Bind event to `close` for `WebSocketClient`.
-          this.$wsc.onclose = function () {
-            // The connection once been lost.
-            if (this.$props.connectState === CON_STATE_RECONNECTING || this.$props.connectState === EVT_LOST) {
+          this.$wsc.onclose = () => {
+            // Stop send ping.
+            stopSendPing();
+
+            // The connection is not closed manually.
+            if (!manuallyClose) {
+
               // should reconnect automatically.
               if (this.$options.reconnect) {
                 // Performs a delayed reconnection.
-                setTimeout(onWebSocketReconnect, this.$options.reconnectDelay);
+                onWebSocketReconnect();
 
                 // Set the states.
                 this.$props.connectState = CON_STATE_RECONNECTING;
                 this.$props.onlineState = LNE_STATE_OFFLINE;
+                return;
               }
 
               // should not reconnect automatically.
               else {
                 this.$props.connectState = CON_STATE_LOST;
                 this.$props.onlineState = LNE_STATE_OFFLINE;
+                return;
               }
             }
 
@@ -217,6 +279,7 @@ export default function (options) {
               this.$props.connectState = CON_STATE_CLOSED;
               this.$props.onlineState = LNE_STATE_OFFLINE;
               this.$props.inited = false;
+              this.manuallyClose = false;
 
               // `close` event fired.
               this.$evt.fire(EVT_CLOSED, {
@@ -226,7 +289,7 @@ export default function (options) {
           };
 
           // Bind event to `message` for `WebSocketClient`.
-          this.$wsc.onmessage = function (evt) {
+          this.$wsc.onmessage = (evt) => {
             let payload = evt.data;
 
             // Check if payload is pong
@@ -245,13 +308,15 @@ export default function (options) {
           };
 
           // Bind event to `error` for `WebSocketClient`.
-          this.$wsc.onerror = function (evt) {
+          this.$wsc.onerror = (evt) => {
             console.error(" *** WebSocket failed to open. ***");
             console.error("     ", evt);
-
-            // Performs a delayed reconnection.
-            if (this.$options.reconnect) {
-              setTimeout(onWebSocketReconnect, this.reconnectDelay);
+            console.log("error and state", this.$props.connectState);
+            if (this.$props.connectState === "connected") {
+              // `lost` event would be triggered.
+              this.$evt.fire(EVT_LOST, {
+                wsc: this.$wsc
+              });
             }
           };
         } catch (err) {
@@ -271,11 +336,23 @@ export default function (options) {
   this.open = initWebSocket;
   this.close = () => {
     if (this.$wsc.readyState === 1) {
+      manuallyClose = true;
       this.$wsc.close();
+    }
+  };
+  this.on = this.$evt.on;
+  this.off = this.$evt.off;
+  this.send = (payload, failedCallback) => {
+    if (this.$wsc.readyState === 1) {
+      this.$wsc.send(payload);
+    } else {
+      console.error(" *** WebSocket is not ready. ***");
+      if (typeof failedCallback === TYPE_FUNCTION) {
+        failedCallback.call(this);
+      }
     }
   };
 
   // auto started tasks.
-  sendPingTask();
   connectionCheckingTask();
 }
